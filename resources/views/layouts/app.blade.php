458960a4a465f618ea8e5c0872c93fa321c99936
KianYen-Yeeee <watermelin0413@gmail.com>
2025-11-22 17:48:56 +0800

    Initial commit: Laravel 10 + Tailwind v3 setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Preorder System</title>
+    @vite('resources/js/app.js')
+</head>
+<body class="bg-gray-100">
+    @yield('content') {{-- testing --}}
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center mt-20">
+        <h1 class="text-4xl font-bold text-blue-600">Tailwind is working!</h1> {{-- for testing --}}
+    </div>
+@endsection
